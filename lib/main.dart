d4bd8b3cf1b8616c7bbd4cec126002c396dfbcf6
lrtzndrsn <lana@itu.dk>
2024-02-27 12:17:01 +0100

    Add ADR
    Add Architectural decision records folder with example markdown file such that the team can document architectural decisions

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have punched the button this many times:',
             ),
             Text(
               '$_counter',
