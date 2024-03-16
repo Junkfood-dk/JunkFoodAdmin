99b44fa2381fbf7cde297af76710d9fc28472023
Allan <asia@itu.dk>
2024-03-16 14:52:48 +0100

    remove redundancy

--- lib/model/allergen_model.dart
+++ lib/model/allergen_model.dart
@@ -6,23 +6,11 @@ class AllergenModel {
   AllergenModel({required this.name});
 
   Map<String, dynamic> toJson() {
-    return {'name': name};
+    return {'allergen_name': name};
   }
 
   static AllergenModel fromJson(Map<String, dynamic> input) {
     return AllergenModel(name: input.containsKey("allergen_name") as String);
     // id: input.containsKey("id") as int);
   }
-
-  Future<void> saveNewAllergen(
-      String name) async {
-    AllergenModel newAllergen = AllergenModel(
-        name: name);
-    var row = await supabase.from("Allergens").insert(newAllergen).select("id");
-    //var id = row[0]['id'];
-    //await supabase.from("Dish_Schedule").insert(
-        //{'id': id, 'date': DateTime.now().toIso8601String()}).select("id");
-  }
-
-
 }
