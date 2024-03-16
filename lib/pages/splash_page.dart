c58b5bcbcc9cbf79b0db62ba07baa7808dc31141
Allan <asia@itu.dk>
2024-03-16 15:06:44 +0100

    missing login?

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -24,7 +24,7 @@ class _SplashPageState extends State<SplashPage> {
 
     final session = supabase.auth.currentSession;
     if (session != null) {
-      Navigator.of(context).pushReplacementNamed('/home');
+      Navigator.of(context).pushReplacementNamed('/login');
     } else {
       Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => const HomePage(),
