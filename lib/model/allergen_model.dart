cd143921029e470bb889949717b42852586db10b
JohanSandager <jsbe@itu.dk>
2024-03-17 15:59:36 +0100

    Add ID to allergenmodel
    Add Id to allergenmodel, as to be able to refrence it when creating a dish. It is only applicable when fetchin an allergen from the db, as the Id is auto assigned on post.

--- lib/model/allergen_model.dart
+++ lib/model/allergen_model.dart
@@ -1,6 +1,7 @@
 class AllergenModel {
+  int id;
   String name;
-  AllergenModel({required this.name});
+  AllergenModel({required this.name, this.id = -1});
 
   Map<String, dynamic> toJson() {
     return {'allergen_name': name};
@@ -10,7 +11,9 @@ class AllergenModel {
     return AllergenModel(
         name: input.containsKey("allergen_name")
             ? input["allergen_name"]
-            : throw Exception("Missing name"));
-    // id: input.containsKey("id") as int);
+            : throw Exception("Missing name"),
+        id: input.containsKey("id")
+            ? input["id"]
+            : throw Exception("Missing id"));
   }
 }
