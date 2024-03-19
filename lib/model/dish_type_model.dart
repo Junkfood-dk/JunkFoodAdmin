7969506063cc043062aeefe0d2a4202119e65fce
JohanSandager <jsbe@itu.dk>
2024-03-19 10:27:00 +0100

    Add dish type model
    Add dish type model to model a dish type, used to categorise dishes for menues.
    
    Co-authored-by: Jonas <kram@itu.dk>

---
+++ lib/model/dish_type_model.dart
@@ -0,0 +1,19 @@
+class DishTypeModel {
+  int id;
+  String type;
+  DishTypeModel({required this.id, required this.type});
+
+  Map<String, dynamic> toJson() {
+    return {'dish_type': type};
+  }
+
+  static DishTypeModel fromJson(Map<String, dynamic> input) {
+    return DishTypeModel(
+        id: input.containsKey('id')
+            ? input['id']
+            : throw Exception("Missing id"),
+        type: input.containsKey('dish_type')
+            ? input['dish_type']
+            : throw Exception("Missing type"));
+  }
+}
