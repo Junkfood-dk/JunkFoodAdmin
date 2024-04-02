a2618b31bd10053c997716518177b7a663add295
Jonas Kramer <kram@itu.dk>
2024-04-02 11:08:10 +0200

    Make dish repository methods
    Co-authored-by: Markus <mbrh@itu.dk>
    Co-authored-by: Lukas <lupa@itu.dk>
    Co-authored-by: Johan <jsbe@itu.dk>
    Co-authored-by: Lauritz <lana@itu.dk>

---
+++ lib/Data/dish_repository.dart
@@ -0,0 +1,31 @@
+import 'package:chefapp/Data/interface_dish_repository.dart';
+import 'package:chefapp/Domain/Model/dish_model.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
+class DishRepository implements IDishRepository {
+  SupabaseClient database;
+  DishRepository({required this.database});
+  @override
+  Future<List<DishModel>> fetchDishOfTheDay() async {
+    return await database
+        .from("Dish_Schedule")
+        .select(
+            "Dish(id, title, description, calories, Dish_type(dish_type), image)")
+        .filter("date", "eq", DateTime.now().toIso8601String())
+        .then((rows) => rows.map((json) => DishModel.fromJson(json)).toList());
+  }
+
+  @override
+  void postDishOfTheDay(
+      String title, String description, int calories, String imageUrl) async {
+    DishModel newDish = DishModel(
+        title: title,
+        description: description,
+        calories: calories,
+        imageUrl: imageUrl);
+    var row = await database.from("Dishes").insert(newDish).select("id");
+    var id = row[0]['id'];
+    await database.from("Dish_Schedule").insert(
+        {'id': id, 'date': DateTime.now().toIso8601String()}).select("id");
+  }
+}
--- lib/Data/interface_dish_repository.dart
+++ lib/Data/interface_dish_repository.dart
@@ -2,5 +2,5 @@ import 'package:chefapp/Domain/Model/dish_model.dart';
 
 abstract interface class IDishRepository {
   Future<List<DishModel>> fetchDishOfTheDay();
-  void postDishOfTheDay();
+  void postDishOfTheDay(String title, String description, int calories, String imageUrl);
 }
