e708fb1e0a87432dfcb69fe7bcd8a59bd9c60119
lrtzndrsn <lana@itu.dk>
2024-04-23 12:37:19 +0200

    Add camera-feature to main
    Adding the camera-feature to main. We have made a repository for images that can used for general purposes when one wants to upload an image. We have integrated the camera button in the UI. There is a button in the post_dish_page for the camera. There have also been created a camera_state_controller to take pictures and a camera_widget that contains the buttons, etc. We make one big juicy commit due to problems with netlify, which made our initial branch cursed and forced us to make a new branch, where we had to copy in all our code. The code has also been tested in camera_widget_test.dart.
    
    Co-authored-by: Markus <mbrh@itu.dk>

---
+++ lib/UI/Widgets/camera_widget.dart
@@ -0,0 +1,74 @@
+import 'package:chefapp/UI/Controllers/camera_state_controller.dart';
+import 'package:chefapp/UI/pages/display_picture_page.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class CameraWidget extends ConsumerWidget {
+  const CameraWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.cameraTitle),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Dispose the CameraStateController
+            ref.watch(cameraStateControllerProvider.notifier).dispose();
+            // Navigate back
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: FutureBuilder<CameraController?>(
+        future: ref.watch(cameraStateControllerProvider.future),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return CameraPreview(
+                snapshot.data!,
+              );
+            } else {
+              return Text('Failed to initialize camera');
+            }
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await ref.watch(cameraStateControllerProvider.future);
+
+            final image = await ref
+                .watch(cameraStateControllerProvider.notifier)
+                .takePicture();
+
+            if (!context.mounted) return;
+
+            final bool satisfiedImage = await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPicturePage(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+
+            if (satisfiedImage) {
+              Navigator.of(context).pop(XFile(image.path));
+            }
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
--- lib/UI/pages/post_dish_page.dart
+++ lib/UI/pages/post_dish_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:camera/camera.dart';
 import 'package:chefapp/Domain/model/allergen_model.dart';
 import 'package:chefapp/Domain/model/category_model.dart';
 import 'package:chefapp/UI/Controllers/allergenes_controller.dart';
@@ -7,6 +8,7 @@ import 'package:chefapp/UI/Controllers/categories_controller.dart';
 import 'package:chefapp/UI/Controllers/dish_of_the_day_controller.dart';
 import 'package:chefapp/UI/Controllers/selected_allergenes_controller.dart';
 import 'package:chefapp/UI/Controllers/selected_categories_controller.dart';
+import 'package:chefapp/UI/Widgets/camera_widget.dart';
 import 'package:chefapp/UI/Widgets/dish_type_dropdown_widget.dart';
 import 'package:chefapp/UI/Widgets/language_dropdown_widget.dart';
 import 'package:chefapp/UI/Widgets/mutable_checkbox_widget.dart';
@@ -30,22 +32,19 @@ class PostDishPage extends HookConsumerWidget {
     newAllergenTextController.addListener(() {
       final text = newAllergenTextController.text;
       String capitalizedValue = text.replaceAllMapped(
-        RegExp(r'(?<=^|\P{L})\p{L}', unicode: true),
-        (match) {
-          String matchedWord = match.group(0)!;
-          if (matchedWord.isNotEmpty) {
-            return matchedWord[0].toUpperCase() + matchedWord.substring(1).toLowerCase();
-          }
-          return matchedWord;
+          RegExp(r'(?<=^|\P{L})\p{L}', unicode: true), (match) {
+        String matchedWord = match.group(0)!;
+        if (matchedWord.isNotEmpty) {
+          return matchedWord[0].toUpperCase() +
+              matchedWord.substring(1).toLowerCase();
         }
-      );
+        return matchedWord;
+      });
       int cursorPosition = newAllergenTextController.selection.baseOffset;
       newAllergenTextController.value = TextEditingValue(
-        text: capitalizedValue,
-        selection: TextSelection.fromPosition(
-          TextPosition(offset: min(cursorPosition, capitalizedValue.length))
-        )
-      );
+          text: capitalizedValue,
+          selection: TextSelection.fromPosition(TextPosition(
+              offset: min(cursorPosition, capitalizedValue.length))));
     });
 
     var newCategoryTextController = useTextEditingController();
@@ -75,22 +74,21 @@ class PostDishPage extends HookConsumerWidget {
                   controller: nameTextController,
                   onChanged: (value) {
                     String capitalizedValue = value.replaceAllMapped(
-                      RegExp(r'(?<=^|\P{L})\p{L}', unicode: true),
-                      (match) {
-                        String matchedWord = match.group(0)!;
-                        if (matchedWord.isNotEmpty) {
-                          return matchedWord[0].toUpperCase() + matchedWord.substring(1).toLowerCase();
-                        }
-                        return matchedWord;
+                        RegExp(r'(?<=^|\P{L})\p{L}', unicode: true), (match) {
+                      String matchedWord = match.group(0)!;
+                      if (matchedWord.isNotEmpty) {
+                        return matchedWord[0].toUpperCase() +
+                            matchedWord.substring(1).toLowerCase();
                       }
-                    );
-                    int cursorPosition = nameTextController.selection.baseOffset;
+                      return matchedWord;
+                    });
+                    int cursorPosition =
+                        nameTextController.selection.baseOffset;
                     nameTextController.value = TextEditingValue(
-                      text: capitalizedValue,
-                      selection: TextSelection.fromPosition(
-                        TextPosition(offset: min(cursorPosition, capitalizedValue.length))
-                      )
-                    );
+                        text: capitalizedValue,
+                        selection: TextSelection.fromPosition(TextPosition(
+                            offset:
+                                min(cursorPosition, capitalizedValue.length))));
                   },
                 ),
                 TextFormField(
@@ -102,16 +100,15 @@ class PostDishPage extends HookConsumerWidget {
                   keyboardType: TextInputType.multiline,
                   onChanged: (value) {
                     String capitalizedValue = value.replaceAllMapped(
-                      RegExp(r'(?<=(?:^|[.!?]\s))\p{L}', unicode: true),
-                      (match) => match.group(0)!.toUpperCase()
-                    );
-                    int cursorPosition = descriptionTextController.selection.baseOffset;
+                        RegExp(r'(?<=(?:^|[.!?]\s))\p{L}', unicode: true),
+                        (match) => match.group(0)!.toUpperCase());
+                    int cursorPosition =
+                        descriptionTextController.selection.baseOffset;
                     descriptionTextController.value = TextEditingValue(
-                      text: capitalizedValue,
-                      selection: TextSelection.fromPosition(
-                        TextPosition(offset: min(cursorPosition, capitalizedValue.length))
-                      )
-                    );
+                        text: capitalizedValue,
+                        selection: TextSelection.fromPosition(TextPosition(
+                            offset:
+                                min(cursorPosition, capitalizedValue.length))));
                   },
                 ),
                 TextFormField(
@@ -126,17 +123,34 @@ class PostDishPage extends HookConsumerWidget {
                       calorieCount.value = int.tryParse(value) ?? 0,
                 ),
                 TextFormField(
-                  validator: (value) {
-                    if (!isValidUrl(value!)) {
-                      return AppLocalizations.of(context)!.invalidURLPromt;
-                    } else {
-                      return null;
-                    }
+                    validator: (value) {
+                      if (!isValidUrl(value!)) {
+                        return AppLocalizations.of(context)!.invalidURLPromt;
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                        label: Text(AppLocalizations.of(context)!
+                            .textFormLabelForImageURL)),
+                    controller: imageTextController),
+                OutlinedButton(
+                  onPressed: () async {
+                    // Navigate to the CameraPage and pass the camera
+                    final XFile image = await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const CameraWidget(),
+                      ),
+                    );
+                    imageTextController.text = image.path;
                   },
-                  decoration: InputDecoration(
-                      label: Text(AppLocalizations.of(context)!
-                          .textFormLabelForImageURL)),
-                  controller: imageTextController,
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      color: Color.fromARGB(
+                          255, 206, 33, 33), // Add border color here
+                    ),
+                  ),
+                  child: Text(AppLocalizations.of(context)!.takePictureLabel),
                 ),
                 MutableCheckboxWidget<AllergenModel>(
                     map: selectedAllergenes,
