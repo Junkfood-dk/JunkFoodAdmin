51767ff318e05ef5c8cf335e1a0bad09070052aa
e138206 <jakob.skov@mastercard.com>
2025-01-11 00:18:26 +0100

    Add comments model and controller

---
+++ lib/data/comments_repository.dart
@@ -0,0 +1,34 @@
+import 'package:chefapp/data/database_provider.dart';
+import 'package:chefapp/domain/model/comment_model.dart';
+import 'package:chefapp/extensions/date_time_ext.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'comments_repository.g.dart';
+
+class CommentRepository {
+  SupabaseClient database;
+
+  CommentRepository({required this.database});
+
+  Future<List<CommentModel>> loadComments([DateTime? date]) async {
+    return await database
+        .from('Comments')
+        .select('Comments()')
+        .filter(
+          'comment_date',
+          'eq',
+          date?.toSupaDate() ?? DateTime.now().toSupaDate(),
+        )
+        .then(
+          (rows) => rows
+              .map((json) => CommentModel.fromJson(json['Comments']))
+              .toList(),
+        );
+  }
+}
+
+@riverpod
+CommentRepository commentRepository(CommentRepositoryRef ref) {
+  return CommentRepository(database: ref.read(databaseProvider));
+}
